1710fa209861579e632707f7f9e56b6b20146178
HSR <hectorsilvarobles@gmail.com>
2020-04-06 13:01:59 -0700

    setting up the foundation of my portfolio

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './navbar.css'
+
+class NavBar extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {}
+    }
+    render(){
+        return (
+            <div className='navbar'></div>
+        )
+    }
+}
+
+export default NavBar
