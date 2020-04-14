a7273fcc746bdf5803e102875de282a5f610d02a
HSR <hectorsilvarobles@gmail.com>
2020-04-14 15:27:28 -0700

    working on the projects section of the portfolio site

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -54,7 +54,48 @@ class Projects extends Component {
                     <h2>Check out what I've built. </h2>
                 </div>
                 <div className='projects-div'>
+                    <div className='projects-carousel'>
+                        <Slider {...settings} asNavFor={this.state.nav1} ref={slider => (this.slider2 = slider)}>
+                            <div className='project-option'>
+                                <h1>Think twice</h1>
+                            </div>
+                            <div className='project-option'>
+                                <h1>Around the world</h1>
+                            </div>
+                            <div className='project-option'>
+                                <h1>Son of Sam</h1>
+                            </div>
+                        </Slider>
+                    </div>
+                    <div className='projects-info'>
+                        <Slider asNavFor={this.state.nav2} ref={slider => (this.slider1 = slider)}>
+                            <div className='project-card'>
+                                <div className='pc-real'>
+                                    <div className='project-image'>
 
+                                    </div>
+                                    <div className='p-info'>
+                                        <h1>COVID-19 Tracker</h1>
+                                        <p>This is the description for the COVID-19 live tracker. This should be at least 3 sentences. So write something simple, short, and good. </p>
+                                        <div className='p-builtwith'>
+
+                                        </div>
+                                        <div className='project-buttons'>
+                                            <button className='view-live'>View Live</button>
+                                            <button className='view-code'>View Code</button>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='project-card'>
+                                <h1>im feeling rough</h1>
+                            </div>
+                            <div className='project-card'>
+                                <h1>Ive got to maintain</h1>
+                            </div>
+                        </Slider>
+                    </div>
                 </div>
             </div>
         )
