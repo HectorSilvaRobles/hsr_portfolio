e4049f7351d1c34c9a3f626478eafb43ab28f7b7
HSR <hectorsilvarobles@gmail.com>
2020-05-30 21:09:19 -0700

    finished site, now working on hosting

--- src/Components/ResumeCTA/ResumeCTA.js
+++ src/Components/ResumeCTA/ResumeCTA.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import './resumecta.css';
+import resumePDF from '../../media/resume.pdf'
 
 class ResumeCTA extends Component {
     render(){
         return (
             <div className='resume-cta'>
-
+                <a href={resumePDF} target='_blank'>
+                    <h1>See My Resume</h1>
+                </a>
             </div>
         )
     }
